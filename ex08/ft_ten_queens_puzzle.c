fd2303d9c5f895e7b4d2fa6f2536f59b4d452915
hshimizu <hisatosi35350@gmail.com>
2023-03-18 23:34:18 +0900

    OK

--- ex08/ft_ten_queens_puzzle.c
+++ ex08/ft_ten_queens_puzzle.c
@@ -6,7 +6,8 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 13:47:48 by hshimizu          #+#    #+#             */
-/*   Updated: 2023/03/18 18:02:45 by hshimizu         ###   ########.fr       */
+/*   Updated: 2023/03/18 23:33:28 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_ten_queens_puzzle(void);
